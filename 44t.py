9f4ba375381f8b3bde996c852036043e52ad9a28
1456evg <141763298+1456evg@users.noreply.github.com>
2024-11-10 20:11:58 +0500

    Update 44t.py

--- 44t.py
+++ 44t.py
@@ -2,11 +2,14 @@ import pandas as pd
 import random
 
 lst = ['robot'] * 10
+
 lst += ['human'] * 10
+
 random.shuffle(lst)
 
-data = pd.DataFrame({'whoAmI': lst})
+data = pd.DataFrame({'whoAmI':lst})
 
-one_hot_encoded = pd.get_dummies(data, columns=['whoAmI'])
+data_one_hot = pd.DataFrame({'robot': [x == 'robot' for x in data['whoAmI']],
+                             'human': [x == 'human' for x in data['whoAmI']]})
 
-print(one_hot_encoded.head())
+print(data_one_hot[['human', 'robot']].head())
